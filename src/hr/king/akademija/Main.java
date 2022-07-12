a8c4f038b4a24756a702038ab311f7a217a28f3a
pavao.vlahovic <pavao.vlahovic@king-ict.hr>
2022-07-12 15:19:00 +0200

    Add some logic

--- src/hr/king/akademija/Main.java
+++ src/hr/king/akademija/Main.java
@@ -5,6 +5,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello 2");
+
+        int i = 0;
+        int j = 10;
+
+        System.out.println(i + j);
     }
 
 }
