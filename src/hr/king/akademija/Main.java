fe3b1b5a0de6f1b317547de5397d87f8f5ebea88
pavao.vlahovic <pavao.vlahovic@king-ict.hr>
2022-07-12 15:21:06 +0200

    Rollback

--- src/hr/king/akademija/Main.java
+++ src/hr/king/akademija/Main.java
@@ -4,12 +4,6 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello world");
-        System.out.println("Hello 2");
-
-        int i = 0;
-        int j = 10;
-
-        System.out.println(i + j);
     }
 
 }
