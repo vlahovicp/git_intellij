4fee7dd7f4c091a546194e5c3213d666970169a7
pavao.vlahovic <pavao.vlahovic@king-ict.hr>
2022-07-13 11:01:40 +0200

    Dodan komentar

--- src/hr/king/akademija/Main.java
+++ src/hr/king/akademija/Main.java
@@ -5,6 +5,10 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
         System.out.println("Hello 2");
+
+        // komentar
+
+        int i = 0;
     }
 
 }
